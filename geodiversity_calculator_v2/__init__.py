dd7e00d148b9fbeb33db0b1fcc8f4784758729af
Márton Pál <pal.marton@inf.elte.hu>
2026-06-01 13:04:26 +0200

    Delete geodiversity_calculator_v2 directory

--- geodiversity_calculator_v2/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Geodiversity Calculator v2.0"""
-
-def classFactory(iface):
-    from .geodiversity_calculator import GeodiversityCalculator
-    return GeodiversityCalculator(iface)
